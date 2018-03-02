847212f54f3103b85caa247bc2f0c7ffde1bee67
Thib Guicherd-Callin <thib@cs.stanford.edu>
2018-03-02 11:07:51 -0800

    Remove dependency on LOCKSS StringUtil isNullString, replaced with Commons Lang 3 StringUtils isEmpty until StringUtil is in lockss-util.

--- src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactStore.java
+++ src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactStore.java
@@ -33,7 +33,7 @@ package org.lockss.laaws.rs.io.storage.warc;
 import com.google.common.io.CountingOutputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.DeferredFileOutputStream;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.yarn.webapp.MimeType;
