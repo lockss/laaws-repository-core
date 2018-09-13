ec4c38a3dbd11b61419636a07d3fe49ebebb4eba
fergaloy <fergaloy@stanford.edu>
2018-09-13 10:35:49 -1000

    Do not attempt to create the JMS consumer if the system property org.lockss.jmsUri is missing.

--- src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactDataStore.java
+++ src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactDataStore.java
@@ -906,6 +906,12 @@ public abstract class WarcArtifactDataStore implements ArtifactDataStore<Artifac
         @Override
         public void run() {
           String uri = System.getProperty(JmsConsumer.SYSPROP_JMS_URI);
+          log.trace("uri: {}", uri);
+
+          if (uri == null || uri.trim().isEmpty()) {
+            return;
+          }
+
           log.info("Establishing JMS connection with {}", uri);
           while (jmsConsumer == null) {
             try {
