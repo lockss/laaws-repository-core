bec24c5f5e83a050cd52a5e1a2f56f66c201301d
Daniel Vargas <dlvargas@stanford.edu>
2019-04-18 15:07:19 -0700

    Removed unused classes

--- src/main/java/org/lockss/laaws/rs/io/WarcFileLockMap.java
+++
@@ -1,9 +0,0 @@
-package org.lockss.laaws.rs.io;
-
-import java.util.concurrent.locks.ReentrantLock;
-
-public class WarcFileLockMap extends LockMap<String> {
-  public WarcFileLockMap() {
-    super(ReentrantLock::new);
-  }
-}
