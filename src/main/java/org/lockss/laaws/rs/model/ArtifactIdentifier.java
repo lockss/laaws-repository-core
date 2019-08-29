6705820297ee632dbcce07f66550d054a9f01243
fergaloy <fergaloy@stanford.edu>
2019-08-29 16:28:19 -0700

    Fixed equals() bugs.

--- src/main/java/org/lockss/laaws/rs/model/ArtifactIdentifier.java
+++ src/main/java/org/lockss/laaws/rs/model/ArtifactIdentifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Board of Trustees of Leland Stanford Jr. University,
+ * Copyright (c) 2017-2019, Board of Trustees of Leland Stanford Jr. University,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -136,7 +136,8 @@ public class ArtifactIdentifier implements Serializable, Comparable<ArtifactIden
 
     @Override
     public boolean equals(Object o) {
-       return this.compareTo((ArtifactIdentifier)o) == 0;
+       ArtifactIdentifier other = (ArtifactIdentifier)o;
+       return other != null && this.compareTo(other) == 0;
     }
 
     /**
