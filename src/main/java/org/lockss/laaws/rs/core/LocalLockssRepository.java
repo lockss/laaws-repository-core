980bb5e0ce95d549daa89e3a9ce1f9bf612f4dca
Daniel Vargas <dlvargas@stanford.edu>
2020-03-02 14:17:59 -0800

    Introduced new constructors of LocalLockssRepository taking multiple base paths.

--- src/main/java/org/lockss/laaws/rs/core/LocalLockssRepository.java
+++ src/main/java/org/lockss/laaws/rs/core/LocalLockssRepository.java
@@ -32,7 +32,6 @@ package org.lockss.laaws.rs.core;
 
 import org.lockss.laaws.rs.io.index.ArtifactIndex;
 import org.lockss.laaws.rs.io.index.LocalArtifactIndex;
-import org.lockss.laaws.rs.io.storage.ArtifactDataStore;
 import org.lockss.laaws.rs.io.storage.local.LocalWarcArtifactDataStore;
 import org.lockss.log.L4JLogger;
 
@@ -40,37 +39,55 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Local filesystem implementation of the LOCKSS Repository API that uses a volatile artifact index.
+ * Local filesystem implementation of {@link LockssRepository}.
  */
 public class LocalLockssRepository extends BaseLockssRepository {
   private final static L4JLogger log = L4JLogger.getLogger();
 
   /**
-   * Takes a base path and persisted index name and instantiates a LocalLockssRepository.
+   * Constructor which takes a base path and name of the local index to persist to disk.
    *
-   * @param basePath
-   *          A {@code File} containing the base path of this LOCKSS repository.
-   * @param persistedIndexName
-   *          A String with the name of the file where to persist the index.
+   * @param basePath           A {@link File} containing the base path of this LOCKSS repository.
+   * @param persistedIndexName A {@link String} with the name of the file where to persist the index.
    */
   public LocalLockssRepository(File basePath, String persistedIndexName) throws IOException {
     index = new LocalArtifactIndex(basePath, persistedIndexName);
     store = new LocalWarcArtifactDataStore(index, new File[]{basePath});
   }
 
-  protected LocalLockssRepository(ArtifactIndex ai, ArtifactDataStore ads) throws IOException {
-    super(ai, ads); /*...*/
+  /**
+   * Constructor which takes a base path and name of the local index to persist to disk.
+   * <p>
+   * Note: The local index is persisted to the first base path.
+   *
+   * @param basePaths          An array of {@link File} containing the base paths of this LOCKSS repository.
+   * @param persistedIndexName A {@link String} with the name of the file where to persist the index.
+   * @throws IOException
+   */
+  public LocalLockssRepository(File[] basePaths, String persistedIndexName) throws IOException {
+    // FIXME: Keeping the persisted LocalArtifactIndex on the first base path
+    index = new LocalArtifactIndex(basePaths[0], persistedIndexName);
+    store = new LocalWarcArtifactDataStore(index, basePaths);
   }
 
   /**
-   * Constructor that takes a local filesystem base path, and an instance of an ArtifactIndex implementation.
+   * Constructor which takes a local filesystem base path, and an instance of an ArtifactIndex implementation.
    *
-   * @param index
-   *          An {@code ArtifactIndex} to use as this repository's artifact index.
-   * @param basePath
-   *          A {@code File} containing the base path of this LOCKSS repository.
+   * @param index    An {@link ArtifactIndex} to use as this repository's artifact index.
+   * @param basePath A {@link File} containing the base path of this LOCKSS repository.
    */
   public LocalLockssRepository(ArtifactIndex index, File basePath) throws IOException {
     super(index, new LocalWarcArtifactDataStore(index, new File[]{basePath}));
+  }
+
+  /**
+   * Constructor which takes a local filesystem base paths, and an instance of an ArtifactIndex implementation.
+   *
+   * @param index     An {@link ArtifactIndex} to use as this repository's artifact index.
+   * @param basePaths An array of {@link File} containing the base path of this LOCKSS repository.
+   * @throws IOException
+   */
+  public LocalLockssRepository(ArtifactIndex index, File[] basePaths) throws IOException {
+    super(index, new LocalWarcArtifactDataStore(index, basePaths));
   }
 }
