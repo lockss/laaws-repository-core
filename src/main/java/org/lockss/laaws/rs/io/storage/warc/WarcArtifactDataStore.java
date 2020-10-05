16243ad2ce063de9bc768a2bbbe9931123e54786
Daniel Vargas <dlvargas@stanford.edu>
2020-10-05 14:17:16 -0700

    Revert "Checkpoint for meeting discussion"
    This reverts commit ece99f34

--- src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactDataStore.java
+++ src/main/java/org/lockss/laaws/rs/io/storage/warc/WarcArtifactDataStore.java
@@ -52,7 +52,6 @@ import org.archive.io.warc.WARCRecord;
 import org.archive.io.warc.WARCRecordInfo;
 import org.archive.util.anvl.Element;
 import org.lockss.laaws.rs.core.ArtifactCache;
-import org.lockss.laaws.rs.core.LockssArtifactExpiredException;
 import org.lockss.laaws.rs.core.LockssNoSuchArtifactIdException;
 import org.lockss.laaws.rs.io.index.ArtifactIndex;
 import org.lockss.laaws.rs.io.storage.ArtifactDataStore;
@@ -1761,14 +1760,9 @@ public abstract class WarcArtifactDataStore implements ArtifactDataStore<Artifac
           return new CompletedFuture<>(artifact);
 
         case EXPIRED:
-          log.warn("Cannot commit an expired artifact [artifactId: {}, state: {}]", artifact.getId(),
-              artifactState.toString());
-          throw new LockssArtifactExpiredException();
-
         case DELETED:
-          log.warn("Cannot commit deleted artifact [artifactId: {}, state: {}]", artifact.getId(),
-              artifactState.toString());
-          throw new LockssNoSuchArtifactIdException();
+          log.warn("Cannot commit deleted or expired artifact [artifactId: {}, state: {}]", artifact.getId(), artifactState.toString());
+          break;
 
         case UNKNOWN:
         default:
@@ -1777,7 +1771,6 @@ public abstract class WarcArtifactDataStore implements ArtifactDataStore<Artifac
       }
     } catch (URISyntaxException e) {
       // This should never happen since storage URLs are internal
-      log.error("Unexpected URISyntaxException");
       throw new IllegalStateException(e);
     }
 
